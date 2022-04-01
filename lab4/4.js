00fc68fc217717e60152046719526bf0cacea945
secreteman1 <max.polenov@gmail.com>
2022-04-01 22:02:23 +0300

    lab4 done

--- lab4/4.js
+++ lab4/4.js
@@ -17,7 +17,14 @@
 */
 
 function get1DArray(arr) {
-    //code here
+    if (arr.length>0){
+    helping=arr.join(' ');  helping=helping.replace(/[\s.,%]/g, ' ')      
+    let helping2=helping.split(' ')
+   for (let i = 0; i < helping2.length; i+=1) {if(!(isNaN(helping2[i]/1))){ helping2[i]=Number(helping2[i])}}
+   return helping2;
+}
+else return arr;
+    
 }
 
 module.exports = get1DArray;
